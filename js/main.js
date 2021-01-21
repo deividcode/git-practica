fe229c6eb8ec152d5557bebf30a20151adb23a1f
deividcode <quinteromda@gmail.com>
2021-01-21 13:34:59 -0300

    Add Objeto Comida

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+const comida = [
+    {
+        nombre: "Manzana",
+        precio: "$3",
+        color: "red"
+    },
+    {
+        nombre: "Banana",
+        precio: "$0.55",
+        color: "yellow"
+    }
+]
+
 console.log("Hola Mundo");
+console.log(comida);
